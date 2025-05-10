ae4db2125b8776bda3f9cb1778a1783d55d7410f
Sandro Reis <consultorsandro@hotmail.com>
2025-05-10 08:09:02 -0300

    "Identifica bandeira de 10 cartões"

--- src/main.rs
+++ src/main.rs
@@ -20,38 +20,23 @@ fn main() {
         return;
     }
 
-    // Identifica a bandeira do cartão com base no primeiro dígito ou nos primeiros dígitos
-    let card_flag = match card_number.chars().next() {
-        Some('4') => "VISA", // Bandeira VISA começa com 4
-        Some('5') => {
-            // Bandeira MASTERCARD começa com 51 a 55
-            if let Ok(prefix) = card_number[..2].parse::<u32>() {
-                if (51..=55).contains(&prefix) {
-                    "MASTERCARD"
-                } else {
-                    "Desconhecida"
-                }
-            } else {
-                "Desconhecida"
-            }
-        }
-        Some('3') => {
-            // Bandeira AMEX começa com 34 ou 37
-            if let Ok(prefix) = card_number[..2].parse::<u32>() {
-                if prefix == 34 || prefix == 37 {
-                    "AMEX"
-                } else {
-                    "Desconhecida"
-                }
-            } else {
-                "Desconhecida"
-            }
-        }
-        Some('6') => "DISCOVER", // Bandeira DISCOVER começa com 6
+    // Identifica a bandeira do cartão com base no primeiro ou nos primeiros dígitos
+    let card_flag = match card_number {
+        _ if card_number.starts_with('4') => "VISA", // Bandeira VISA começa com 4
+        _ if card_number.len() >= 2 && (51..=55).contains(&card_number[..2].parse::<u32>().unwrap_or(0)) => "Mastercard", // Mastercard entre 51 e 55
+        _ if card_number.len() >= 4 && (2221..=2720).contains(&card_number[..4].parse::<u32>().unwrap_or(0)) => "Mastercard", // Mastercard entre 2221 e 2720
+        _ if card_number.starts_with("4011") || card_number.starts_with("4312") || card_number.starts_with("4389") => "ELO", // ELO
+        _ if card_number.len() >= 2 && (card_number.starts_with("34") || card_number.starts_with("37")) => "American Express", // American Express
+        _ if card_number.starts_with("6011") || card_number.starts_with("65") || (644..=649).contains(&card_number[..3].parse::<u32>().unwrap_or(0)) => "Discover", // Discover
+        _ if card_number.starts_with("6062") => "Hipercard", // Hpercard
+        _ if card_number.len() >= 2 && (card_number.starts_with("30") || card_number.starts_with("36")) => "Diners Club", // Diners Club
+        _ if card_number.len() >= 4 && (card_number.starts_with("2014") || card_number.starts_with("2149")) => "EnRoute", // EnRoute
+        _ if card_number.starts_with("35") => "JCB", // JCB
+        _ if card_number.starts_with("86") => "VOYAGER", // VOYAGER
+        _ if card_number.starts_with("50") => "Aura", // Aura
         _ => "Desconhecida", // Para outros casos, bandeira desconhecida
     };
 
     // Exibe a bandeira do cartão
     println!("[A bandeira do número do cartão é: {} .]", card_flag);
 }
-
